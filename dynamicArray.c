9cac583bff51b910bc821268274b64a5aacc1ec5
CN-John <141564422+CN-John@users.noreply.github.com>
2023-08-25 02:53:51 +0800

    Update dynamicArray.c

--- dynamicArray.c
+++ dynamicArray.c
@@ -2,36 +2,36 @@
 #include<stdlib.h>
 #include<error.h>
 
-typedef struct Array
+typedef struct dynamicArray  //动态数组又名顺序表
 {
-	int* arr;
-	int size;
-	int capacity;
-}Array;
+	int* arr;  //存放整型数据
+	int size;   //数组当前大小
+	int capacity;  //容量
+}dynamicArray;
 
-void array_Init(Array* da, int capacity)
+void dynamicArray_Init(dynamicArray* da, int capacity)  //初始化数组
 {
 	da->arr = (int*)malloc(sizeof(int)*da->capacity);
 	da->size = 0;
 	da->capacity = capacity;
 }
 
-void array_Append(Array* da, int value)
+void dynamicArray_Append(dynamicArray* da, int value)  //追加元素
 {
-	if(da->size == da->capacity){
+	if(da->size == da->capacity){  			//自动扩容
 		da->capacity *= 2;
 		da->arr = (int*)realloc(da->arr,sizeof(int)*da->capacity);
 	}
 	da->arr[da->size++] = value;
 }
 
-void array_Insert(Array* da, int sub, int value)
+void dynamicArray_Insert(dynamicArray* da, int sub, int value) //插入元素
 {
 	if(sub > da->size || sub < 0){
 		perror("Insert:sub error");
 		exit(0);
 	}
-	else if(sub < da->size){
+	else if(sub < da->size){                              //自动扩容
 		if(da->size == da->capacity){
 			da->capacity *= 2;
 			da->arr = (int*)realloc(da->arr,sizeof(int)*da->capacity);
@@ -43,10 +43,10 @@ void array_Insert(Array* da, int sub, int value)
 		da->arr[sub] = value;
 		da->size++;
 	}
-	else if(sub == da->size) array_Append(da, value);
+	else if(sub == da->size) dynamicArray_Append(da, value);
 }
 
-void array_Delete(Array* da, int sub)
+void dynamicArray_Delete(dynamicArray* da, int sub)       //按位置删除元素
 {
 	if(sub > da->size - 1 || sub < 0){
 		perror("Delete:sub error");
@@ -60,7 +60,7 @@ void array_Delete(Array* da, int sub)
 	da->size--;
 }
 
-void array_Print(Array* da)
+void dynamicArray_Print(dynamicArray* da)             //遍历数组
 {
 	int i = 0;
 	for(i = 0; i < da->size; i++){
@@ -69,12 +69,12 @@ void array_Print(Array* da)
 	printf("\n");
 }
 
-int array_getSize(Array* da)
+int dynamicArray_getSize(dynamicArray* da)            //获取数组当前尺寸
 {
 	return da->size;
 }
 
-int array_getValue(Array* da, int sub)
+int dynamicArray_getValue(dynamicArray* da, int sub)   //根据位置获取值
 {
 	if(sub > da->size - 1 || sub < 0){
 		perror("get sub error");
@@ -83,7 +83,7 @@ int array_getValue(Array* da, int sub)
 	return da->arr[sub];
 }
 
-int array_findValue(Array* da, int value)
+int dynamicArray_findValue(dynamicArray* da, int value)   //根据元素寻找位置
 {
 	int i = 0;
 	for(i = 0; i < da->size - 1; i++){
@@ -94,27 +94,27 @@ int array_findValue(Array* da, int value)
 	return -1;
 }
 
-void array_Destroy(Array* da)
+void dynamicArray_Destroy(dynamicArray* da)  //清除动态数组
 {
 	free(da->arr);
 }
 
 int main(int argc, char* argv[])
 {
-	Array A;
-	array_Init(&A, 6);
-	array_Append(&A, 1);
-	array_Append(&A, 2);
-	array_Append(&A, 3);
-	array_Append(&A, 4);
-	array_Delete(&A, 1);
-	array_Delete(&A, 0);
-	array_Insert(&A, 0, 10);
-	array_Print(&A);
-	printf("%d\n",array_getSize(&A));
-	printf("%d\n", array_getValue(&A, 1));
-	printf("%d\n",array_findValue(&A, 1));
-	array_Destroy(&A);
+	dynamicArray A;
+	dynamicArray_Init(&A, 6);
+	dynamicArray_Append(&A, 1);
+	dynamicArray_Append(&A, 2);
+	dynamicArray_Append(&A, 3);
+	dynamicArray_Append(&A, 4);
+	dynamicArray_Delete(&A, 1);
+	dynamicArray_Delete(&A, 0);
+	dynamicArray_Insert(&A, 0, 10);
+	dynamicArray_Print(&A);
+	printf("%d\n",dynamicArray_getSize(&A));
+	printf("%d\n", dynamicArray_getValue(&A, 1));
+	printf("%d\n",dynamicArray_findValue(&A, 1));
+	dynamicArray_Destroy(&A);
     return 0;
 }
 
