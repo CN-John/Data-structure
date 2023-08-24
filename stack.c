1b0e22b695e94e0da64584d32d5d5157b5f56fb2
CN-John <141564422+CN-John@users.noreply.github.com>
2023-08-25 02:06:52 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,44 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct stack
+typedef struct stack //栈结构体
 {
-	int* data;
-	int top;
-	int capacity;
+	int* data;    //数组原型，存放整型数据
+	int top;      //模拟栈顶指针，实现栈的核心
+	int capacity; //栈容量
 }Stack;
 
-void initStack(Stack* sak, int capa)
+void initStack(Stack* sak, int capa)  //初始化栈
 {
 	sak->data = (int*)malloc(sizeof(int)*capa);
 	sak->capacity = capa;
 	sak->top = 0;
 }
 
-int stackIsFull(const Stack* sak)
+int stackIsFull(const Stack* sak)  //判断栈满
 {
 	return sak->top == sak->capacity;
 }
 
-int stackIsEmpty(const Stack* sak)
+int stackIsEmpty(const Stack* sak)  //判断栈空
 {
 	return sak->top == 0;
 }
 
-int stackPush(Stack* sak, int value)
+int stackPush(Stack* sak, int value)  //压栈
 {
 	if(stackIsFull(sak)) return 0;
 	sak->data[sak->top++] = value;
 	return 1;
 }
 
-int stackTop(Stack* sak)
+int stackTop(Stack* sak)  //返回栈顶元素，不弹出
 {
 	if(stackIsEmpty(sak)) return 0;
 	return sak->data[sak->top - 1];
 }
 
-int stackPop(Stack* sak)
+int stackPop(Stack* sak)  //弹栈
 {
 	if(stackIsEmpty(sak)) return 0;
 	int value;
@@ -46,7 +46,7 @@ int stackPop(Stack* sak)
 	return value;
 }
 
-void printStack(Stack* sak)
+void printStack(Stack* sak)  //遍历栈元素
 {
 	Stack* st = sak;
 	while(st->top != 0){
