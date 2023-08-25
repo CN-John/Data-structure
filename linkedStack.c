c48e436f293af9501760bd8fd25cabab37ab3d92
CN-John <141564422+CN-John@users.noreply.github.com>
2023-08-25 21:12:21 +0800

    Create linkedStack.c

---
+++ linkedStack.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<error.h>
+
+typedef struct StackNode
+{
+	int data;
+	struct StackNode* pNext;
+}StackNode;
+
+void createHeadNode(StackNode** phead)
+{
+	*phead = NULL;
+}
+
+StackNode* createStackNode(int value)
+{
+	StackNode* newnode = (StackNode*)malloc(sizeof(StackNode));
+	if(newnode == NULL){
+		perror("create StackNode error!\n");
+		exit(1);
+	}
+	newnode->data = value;
+	newnode->pNext = NULL;
+	return newnode;
+}
+
+void pushStackNode(StackNode** phead, int value)
+{
+	if(*phead){
+		StackNode* tmp = createStackNode(value);
+		tmp->pNext = *phead;
+		*phead = tmp;
+	}else{
+		*phead = createStackNode(value);
+	}
+}
+
+int linked_stackIsEmpty(StackNode** phead)
+{
+	if(!*phead) return 1;
+	else return 0;
+}
+
+int topLinkedStack(StackNode** phead)
+{
+	if(*phead == NULL){
+		perror("LinkedStack is empty");
+		exit(1);
+	}
+	return (*phead)->data;
+}
+
+int popLinkedStack(StackNode** phead)
+{
+	if(*phead){
+		StackNode* tmp = *phead;
+		*phead = tmp->pNext;
+		int temp = tmp->data;
+		free(tmp);
+		return temp;
+	}else{
+		perror("LinkedStack is empty");
+		exit(1);
+	}
+}
+
+int getLinkedStackLength(StackNode** phead)
+{
+	int temp = 0;
+	StackNode* tmp = *phead;
+	if(tmp == NULL) return 0;
+	while(tmp){
+		temp++;
+		tmp = tmp->pNext;
+	}
+	return temp;
+}
+
+void printLinkedStack(StackNode** phead)
+{
+	StackNode* tmp = *phead;
+	if(tmp == NULL){
+		printf("LinkedStack is empty!\n");
+		return;
+	}
+	while(tmp){
+		printf("%d\n",tmp->data);
+		tmp = tmp->pNext;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	StackNode* phead;
+	createHeadNode(&phead);
+	pushStackNode(&phead,11);
+	pushStackNode(&phead,22);
+	pushStackNode(&phead,33);
+	pushStackNode(&phead,44);
+	pushStackNode(&phead,55);
+	printLinkedStack(&phead);
+	printf("%d\n",topLinkedStack(&phead));
+	printf("%d\n",popLinkedStack(&phead));
+	printLinkedStack(&phead);
+	printf("%d\n",getLinkedStackLength(&phead));
+    return 0;
+}
